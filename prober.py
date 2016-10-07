1eae494e915bd183269b0be47be4e42246728b4a
Shweta <shwayytha@gmail.com>
2016-10-07 15:31:22 -0400

    finish prober according to instructions

--- prober.py
+++ prober.py
@@ -7,6 +7,7 @@ Large Scale Web Apps, Fall 2016
 import sys
 import time
 import urllib.request
+import urllib.error
 
 
 
@@ -14,10 +15,13 @@ def probe_server(url):
     request = urllib.request.Request(url)
     try:
         response = urllib.request.urlopen(request).status
-        print(response)
         return response
+    except urllib.error.HTTPError:
+        return -1
     except urllib.error.URLError:
-            return str(-1)
+        return -1
+    except Exception:
+        return -1
 
 def get_vars():
     if (len(sys.argv) < 3):
@@ -28,15 +32,16 @@ def get_vars():
 def main():
     url, samples = get_vars()
     samples_file = open(samples, 'w')
-    samples_file.write("URL = " + url)
+    samples_file.write("URL = " + url + "\n")
     current_time = int(time.time())
     while True: # infinite loop until interrupted
         try:
             if (int(time.time()) - current_time >= 30):
                 response = probe_server(url) 
-                #print(response)
-                write_line = str(int(time.time())) + "," + str(response) + "\n"
+                print(response)
+                write_line = str(int(time.time())) + ", " + str(response) + "\n"
                 samples_file.write(write_line)
+                samples_file.flush() # prints immediately to file
                 current_time = int(time.time())                   
         except (KeyboardInterrupt, SystemExit):
                 samples_file.close()
